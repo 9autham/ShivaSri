131294d1a86c81c255b1fe2df8373433d90b5116
Gautham <gautam9.2016@gmail.com>
2024-09-21 11:22:09 +0530

    Give meaning full AboutUs

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,8 @@ function About() {
   </div>
   <div className="about-description">
     <h2>Hello, I’m Photosri22.</h2>
-    <p>I’m a photographer living in Mahammadabad Mahabubnagar. I am a fan of Varun Adhitya, design, and arts. I’m also interested in travel and movies.</p>
-    <p>I've dedicated over six years to capturing the beauty of life's most precious moments. With a keen eye for detail and a love for design, I specialize in wedding photography, family portraits, events, and commercial films. My goal is to create timeless images that reflect your unique story and evoke lasting emotions. Let's collaborate to turn your vision into reality and create memories that will be cherished for generations to come.</p>
-    <p>As a passionate photographer, I love capturing moments that tell stories. If you enjoy my work, I invite you to explore more on my Instagram, where you'll find my latest projects, behind-the-scenes moments, and creative journeys. Don't forget to follow along for updates, and feel free to reach out to me through any of my social media handles—I’d love to connect and hear from you!</p>
+    <p>I’m a photographer based in Mahammadabad, Mahabubnagar, with over six years of experience capturing life’s most precious moments. My expertise spans wedding photography, family portraits, events, and commercial films, with a focus on creating timeless images that reflect unique stories and emotions. Whether it's a special occasion or a commercial project, I’m passionate about turning visions into reality.</p>
+    <p>If you enjoy my work, I invite you to explore my Instagram for the latest projects, behind-the-scenes moments, and creative journeys. Feel free to follow me for updates and reach out via my social media handles—I’d love to connect!</p>
     </div>
 </div>
 
