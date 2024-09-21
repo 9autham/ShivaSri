db01e874c5adf005d5e249c5e0f5a2322604373c
Gautham <gautam9.2016@gmail.com>
2024-09-21 10:28:11 +0530

    Updating About Us Information

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,9 @@ function About() {
   <div className="about-description">
     <h2>Hello, I’m Photosri22.</h2>
     <p>I’m a photographer living in Mahammadabad Mahabubnagar. I am a fan of Varun Adhitya, design, and arts. I’m also interested in travel and movies.</p>
-    <p>You can view my photos with a click on the button above.</p>
-    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Pellentesque elementum dui bibendum elementum fringilla curae id quisque sapien. Phasellus tempus ante et ultricies; fermentum facilisis aliquam. Sapien accumsan natoque nisi; mi egestas vestibulum inceptos pretium. Imperdiet adipiscing non mattis dignissim fringilla. Ad volutpat vel dolor dignissim cubilia sociosqu curabitur; est nisi.
-
-Ultricies phasellus augue blandit senectus nunc faucibus fusce. Conubia aliquet hendrerit pharetra dictumst malesuada auctor ullamcorper. Eu suscipit fames odio mattis ridiculus turpis feugiat imperdiet. Aliquam himenaeos est mus aptent porta, non nibh sagittis. Praesent convallis ac diam ultrices in. Parturient consequat etiam ex arcu ullamcorper aliquam velit magnis. Sit non justo laoreet posuere luctus nisl non. Molestie hac lorem proin himenaeos; vulputate tempor aenean.</p>
-  </div>
+    <p>I've dedicated over six years to capturing the beauty of life's most precious moments. With a keen eye for detail and a love for design, I specialize in wedding photography, family portraits, events, and commercial films. My goal is to create timeless images that reflect your unique story and evoke lasting emotions. Let's collaborate to turn your vision into reality and create memories that will be cherished for generations to come.</p>
+    <p>As a passionate photographer, I love capturing moments that tell stories. If you enjoy my work, I invite you to explore more on my Instagram, where you'll find my latest projects, behind-the-scenes moments, and creative journeys. Don't forget to follow along for updates, and feel free to reach out to me through any of my social media handles—I’d love to connect and hear from you!</p>
+    </div>
 </div>
 
     </div>
